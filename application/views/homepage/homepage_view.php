689d1250010b6eb838ef05d3cd1c09122007ac7e
Ram Emerson Goria <goria.reg@gmail.com>
2023-04-07 00:06:06 +0800

    Added Services Section

--- application/views/homepage/homepage_view.php
+++ application/views/homepage/homepage_view.php
@@ -9,16 +9,16 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="#hero">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About Me</a>
+                        <a class="nav-link" href="#about">About Me</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Skills</a>
+                        <a class="nav-link" href="#skills">Skills</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Services</a>
+                        <a class="nav-link" href="#services">Services</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Portfolio</a>
@@ -110,3 +110,128 @@
     </div>
 </section>
 <!-- ABOUT ME END -->
+
+<!-- SKILLS START -->
+<section id="skills">
+    <div class="container">
+        <div class="section-title">
+            <h3 class="text-center fw-bold main-title">My Skills</h3>
+            <p class="text-center fw-bold sub-title">My skill</p>
+        </div>
+        <div class="row gap-3 justify-content-center">
+            <div class="col-4 col-lg-2 card text-center p-3 skill">
+                <img src="<?php echo base_url('resources/icons/photoshop.png') ?>" class="card-img-top" alt="Photoshop">
+                <div class="card-body">
+                    <p class="card-text fw-bold skill-text">Adobe Photoshop</p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-2 card text-center p-3 skill">
+                <img src="<?php echo base_url('resources/icons/word.png') ?>" class="card-img-top" alt="Word">
+                <div class="card-body">
+                    <p class="card-text fw-bold skill-text">Microsoft Word</p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-2 card text-center p-3 skill">
+                <img src="<?php echo base_url('resources/icons/powerpoint.png') ?>" class="card-img-top" alt="PowerPoint">
+                <div class="card-body">
+                    <p class="card-text fw-bold skill-text">Microsoft PowerPoint</p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-2 card text-center p-3 skill">
+                <img src="<?php echo base_url('resources/icons/html-5.png') ?>" class="card-img-top" alt="HTML">
+                <div class="card-body">
+                    <p class="card-text fw-bold skill-text">HTML 5</p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-2 card text-center p-3 skill">
+                <img src="<?php echo base_url('resources/icons/css-3.png') ?>" class="card-img-top" alt="CSS">
+                <div class="card-body">
+                    <p class="card-text fw-bold skill-text">CSS 3</p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-2 card text-center p-3 skill">
+                <img src="<?php echo base_url('resources/icons/javascript.png') ?>" class="card-img-top" alt="JavaScript">
+                <div class="card-body">
+                    <p class="card-text fw-bold skill-text">JavaScript</p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-2 card text-center p-3 skill">
+                <img src="<?php echo base_url('resources/icons/php.png') ?>" class="card-img-top" alt="PHP">
+                <div class="card-body">
+                    <p class="card-text fw-bold skill-text">PHP</p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-2 card text-center p-3 skill">
+                <img src="<?php echo base_url('resources/icons/bootstrap.png') ?>" class="card-img-top" alt="Bootstrap">
+                <div class="card-body">
+                    <p class="card-text fw-bold skill-text">Bootstrap 5</p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-2 card text-center p-3 skill">
+                <img src="<?php echo base_url('resources/icons/codeigniter.png') ?>" class="card-img-top" alt="CodeIgniter">
+                <div class="card-body">
+                    <p class="card-text fw-bold skill-text">CodeIgniter 3</p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-2 card text-center p-3 skill">
+                <img src="<?php echo base_url('resources/icons/php.png') ?>" class="card-img-top" alt="PHP">
+                <div class="card-body">
+                    <p class="card-text fw-bold skill-text">PHP</p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-2 card text-center p-3 skill">
+                <img src="<?php echo base_url('resources/icons/android-studio.png') ?>" class="card-img-top" alt="Android">
+                <div class="card-body">
+                    <p class="card-text fw-bold skill-text">Android Studio (Kotlin)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- SKILLS END -->
+
+<!-- SERVICES START -->
+<section id="services">
+    <div class="container">
+        <div class="section-title">
+            <h3 class="text-center fw-bold main-title">My Services</h3>
+            <p class="text-center fw-bold sub-title">services</p>
+        </div>
+        <div class="row gap-3 justify-content-center">
+            <div class="col-4 col-lg-3 card text-center p-3 bg-transparent">
+                <div class="card-img-top d-flex justify-content-center">
+                    <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_ctaacxzb.json"  background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></lottie-player>
+                </div>
+                <div class="card-body skill-text">
+                    <h3 class="card-title fw-bold">Web Application Developer</h3>
+                    <p class="card-text">
+                        I have a passion to create web applications that can help users interact easily to the website.
+                    </p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-3 card text-center p-3 bg-transparent">
+                <div class="card-img-top d-flex justify-content-center">
+                    <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_Zo9feTljq8.json"  background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></lottie-player>
+                </div>
+                <div class="card-body skill-text">
+                    <h3 class="card-title fw-bold">Mobile Application Developer</h3>
+                    <p class="card-text">
+                        I have a passion to create mobile applications that can help users interact easily to the application.
+                    </p>
+                </div>
+            </div>
+            <div class="col-4 col-lg-3 card text-center p-3 bg-transparent">
+                <div class="card-img-top d-flex justify-content-center">
+                    <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_rkyfvrzn.json"  background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></lottie-player>
+                </div>
+                <div class="card-body skill-text">
+                    <h3 class="card-title fw-bold">Web and Mobile Designer</h3>
+                    <p class="card-text">
+                        To design website and mobile to make it more interactive and easy to use and understand to users.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- SERVICES END -->
--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -4,6 +4,9 @@
 <!-- Typed.js CDN -->
 <script src="https://unpkg.com/typed.js@2.0.15/dist/typed.umd.js"></script>
 
+<!-- LottieFiles CDN -->
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+
 <!-- Custom Javascript -->
 <script src="<?php echo base_url(); ?>assets/js/main.js"></script>
 
